0fa34ba9e17a1838f7c44ee57ce7b90f0b18fe82
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-10-15 15:21:16 +0900

    refactor : split into subcomponents

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,61 @@
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Image, Box, Button, Flex, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../configs/routes";
 
 export const Header = () => {
-  const displayLogout = true;
-  const handleLogout = () => {};
-
-  const navigate = useNavigate();
-  const handleRedirectToHome = () => {
-    navigate("/");
-  };
-
   return (
     <Flex
       as="header"
       w={"full"}
       h={"16"}
+      flexShrink={0}
       px={"4"}
       alignItems={"center"}
       borderBottom={"1px"}
       borderColor={"gray.200"}
     >
-      <Flex gap={"6"} alignItems={"center"}>
-        <Button variant={"unstyled"} onClick={handleRedirectToHome}>
-          <Text>KUSITMS +</Text>
-        </Button>
-        <Text fontWeight={"bold"}>ADMIN</Text>
+      <Flex gap={"5"} alignItems={"center"}>
+        <HomeButton />
+        <AdminName />
       </Flex>
       <Box flex={1}></Box>
+      <LogoutButton />
+    </Flex>
+  );
+};
+
+const HomeButton = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <Button variant={"unstyled"} onClick={handleClick}>
+      <Image src={"logo.svg"}></Image>
+    </Button>
+  );
+};
+
+const AdminName = () => {
+  return <Text fontWeight={"bold"}>ADMIN</Text>;
+};
+
+const LogoutButton = () => {
+  const displayLogout = true;
+  // TODO: implement logout
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    navigate(ROUTES.AUTHENTICATION);
+  };
+
+  return (
+    <>
       {displayLogout ? (
         <Button w={"40"} variant={"ghost"} onClick={handleLogout}>
           Logout
         </Button>
       ) : null}
-    </Flex>
+    </>
   );
 };
