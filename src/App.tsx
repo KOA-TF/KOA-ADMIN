48d11517d0332e13b3e541312dab35fd26608be3
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-10-15 15:24:58 +0900

    fix : update page imports

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,19 @@ import { ROUTES } from "./configs/routes";
 import { AddCurriculum } from "./pages/AddCurriculum";
 import { AddMember } from "./pages/AddMember";
 import { Authentication } from "./pages/Authentication";
-import { ManageCommentPage } from "./pages/ManageCommentPage";
-import { ManageMemberPage } from "./pages/ManageMemberPage";
+import { ManageComment } from "./pages/ManageComment";
+import { ManageMember } from "./pages/ManageMember";
 
 export const App = () => {
   return (
     <Routes>
       <Route path={ROUTES.AUTHENTICATION} element={<Authentication />} />
       <Route element={<ProtectedRoute />}>
-        <Route path={ROUTES.ROOT} element={<ManageMemberPage />} />
+        <Route path={ROUTES.ROOT} element={<ManageMember />} />
         <Route path={ROUTES.ADD_CURRICULUM} element={<AddCurriculum />} />
         <Route path={ROUTES.ADD_MEMBER} element={<AddMember />} />
-        <Route path={ROUTES.MANAGE_COMMENT} element={<ManageCommentPage />} />
-        <Route
-          path={ROUTES.MANAGE_MEMBER}
-          element={<ManageMemberPage />}
-          index
-        />
+        <Route path={ROUTES.MANAGE_COMMENT} element={<ManageComment />} />
+        <Route path={ROUTES.MANAGE_MEMBER} element={<ManageMember />} index />
         <Route />
       </Route>
     </Routes>
