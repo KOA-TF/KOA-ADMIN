f47adc15941ea49c741f9158f75a183a6512ad69
Thomas Min <tommin231@gmail.com>
2023-12-18 01:31:19 +0900

    feat : update search input to set query param

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,17 +1,37 @@
 import { SearchIcon } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { StringParam, useQueryParam, withDefault } from "use-query-params";
 
 export function SearchInput() {
-  const [value, onChange] = useState("");
+  const [value, setValue] = useState("");
+
+  const [, setSearchParam] = useQueryParam(
+    "search",
+    withDefault(StringParam, ""),
+  );
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setSearchParam(value);
+    }
+  };
+
+  useEffect(() => {
+    if (value === "") {
+      setSearchParam("");
+    }
+  }, [setSearchParam, value]);
 
   return (
-    <div className="max-w-xs">
+    <div className="relative flex w-full max-w-sm items-center">
       <input
+        className="w-full rounded-lg bg-gray-100 px-5 py-4 pl-14 text-lg outline-blue-600 placeholder:text-gray-400"
         placeholder="검색하기"
         value={value}
-        onChange={(e) => onChange(e.target.value)}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
-      <SearchIcon size={24} />
+      <SearchIcon className="absolute left-5" size={24} />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App.tsx";
 import { ROUTES } from "./constants/routes.ts";
-import "./index.css";
+import { QueryParamProvider } from "use-query-params";
+import { ReactRouter6Adapter } from "use-query-params/adapters/react-router-6";
 import { RecoilRoot } from "recoil";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +16,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter basename={ROUTES.BASE}>
-          <App />
+          <QueryParamProvider adapter={ReactRouter6Adapter}>
+            <App />
+          </QueryParamProvider>
         </BrowserRouter>
       </QueryClientProvider>
     </RecoilRoot>
